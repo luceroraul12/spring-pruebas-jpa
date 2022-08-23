90b22cfb12337fee8c63341f2cf4f9d8da472a43
Homitowen <pycgames@gmail.com>
2022-08-22 22:40:57 -0300

    cree un metodo con nullable para ver el comportamiento

--- src/main/java/com/example/springjpa/ServicioRepository.java
+++ src/main/java/com/example/springjpa/ServicioRepository.java
@@ -1,8 +1,14 @@
 package com.example.springjpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ServicioRepository extends JpaRepository<Servicio, Long> {
+    List<Servicio> findByIdAndNombre(@Nullable Long id, @Nullable String nombre);
+
+
 }
