9522caa64d9cc7cce8d8400ea7ed65606a051b65
Homitowen <pycgames@gmail.com>
2022-08-26 16:09:28 -0300

    add. metodos básicos

--- src/main/java/com/example/springjpa/services/Mensualidades.java
+++ src/main/java/com/example/springjpa/services/Mensualidades.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Service
 public interface Mensualidades {
 
-
+    List<Servicio> getServiciosCargados();
+    List<Pago> getPagosCargados();
 
 }
--- src/main/java/com/example/springjpa/services/imp/MensualidadesImp.java
+++ src/main/java/com/example/springjpa/services/imp/MensualidadesImp.java
@@ -1,6 +1,33 @@
 package com.example.springjpa.services.imp;
 
+import com.example.springjpa.models.Pago;
+import com.example.springjpa.models.Servicio;
+import com.example.springjpa.repository.PagoRepository;
+import com.example.springjpa.repository.ServicioRepository;
 import com.example.springjpa.services.Mensualidades;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class MensualidadesImp implements Mensualidades {
+
+    @Autowired
+    private ServicioRepository servicioRepository;
+    @Autowired
+    private PagoRepository pagoRepository;
+
+    public MensualidadesImp(ServicioRepository servicioRepository, PagoRepository pagoRepository) {
+        this.servicioRepository = servicioRepository;
+        this.pagoRepository = pagoRepository;
+    }
+
+    @Override
+    public List<Servicio> getServiciosCargados() {
+        return servicioRepository.findAll();
+    }
+
+    @Override
+    public List<Pago> getPagosCargados() {
+        return pagoRepository.findAll();
+    }
 }
